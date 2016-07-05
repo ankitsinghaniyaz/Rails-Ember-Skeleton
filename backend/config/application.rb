d491ea226e27e88b07d54fc9601397e031437b69
Ankit Singhaniya <ankit.singhaniyaz@gmail.com>
2016-07-05 19:36:03 +0530

    replace rails backend with api only backend

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -7,30 +7,36 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module Backend
-   class Application < Rails::Application
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
 
-      # Disabling asset pipeline
-      config.assets.enabled = false
-      config.generators do |g|
-        g.assets false
-      end
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # config.time_zone = 'Central Time (US & Canada)'
 
-      # We want to set up a custom logger which logs to STDOUT.
-      # Docker expects your application to log to STDOUT/STDERR and to be ran
-      # in the foreground.
-      config.log_level = :debug
-      config.log_tags  = [:subdomain, :uuid]
-      config.logger    = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # config.i18n.default_locale = :de
 
-      # Since we're using Redis for Sidekiq, we might as well use Redis to back
-      # our cache store. This keeps our application stateless as well.
-      config.cache_store = :redis_store, ENV['CACHE_URL'],
-                           { namespace: 'drkiq::cache' }
+    # We want to set up a custom logger which logs to STDOUT.
+    # Docker expects your application to log to STDOUT/STDERR and to be ran
+    # in the foreground.
+    config.log_level = :debug
+    config.log_tags  = [:subdomain, :uuid]
+    config.logger    = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
 
-      # If you've never dealt with background workers before, this is the Rails
-      # way to use them through Active Job. We just need to tell it to use Sidekiq.
-      config.active_job.queue_adapter = :sidekiq
+    # Since we're using Redis for Sidekiq, we might as well use Redis to back
+    # our cache store. This keeps our application stateless as well.
+    config.cache_store = :redis_store, ENV['CACHE_URL'],
+                         { namespace: 'drkiq::cache' }
 
-      # ...
-   end
+    # If you've never dealt with background workers before, this is the Rails
+    # way to use them through Active Job. We just need to tell it to use Sidekiq.
+    config.active_job.queue_adapter = :sidekiq
+
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
+  end
 end
